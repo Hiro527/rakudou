7783743703f6f29491499cff1baef71a2c40a669
Hiro <hiro527.dev@gmail.com>
2021-12-31 02:51:27 +0900

    [Change] 完成

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # rakudou
-落道 - YouTube mp3 Downloader
+落道 - YouTube Downloader
+
+## 使い方
+1. リポジトリをクローンする: `git clone https://github.com/Hiro527/rakudou.git`
+2. 必要なパッケージをインストールする: `npm ci`
+3. コマンドを実行する: `node index.js [動画のURL] (-mp3: mp3ファイルへの音声の変換を行いたい場合はつけてください)`
+
+## 必要条件
+- Node.js >= 16.13.0
+- npm >= 8.1.2
---
+++ index.js
@@ -0,0 +1,103 @@
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+const child_process = require('child_process');
+const { exit } = require('process');
+const readline = require('readline');
+
+const packageInfo = require('./package.json');
+
+/**
+ * @type {String}
+ */
+const url = process.argv[2];
+
+const mode = process.argv[3] ?? 'mp4'
+
+console.log(`
+######     ##     ###  ##  ##   ##  #####     #####   ##   ##
+ ##  ##   ####     ##  ##  ##   ##   ## ##   ##   ##  ##   ##
+ ##  ##  ##  ##    ## ##   ##   ##   ##  ##  ##   ##  ##   ##
+ #####   ##  ##    ####    ##   ##   ##  ##  ##   ##  ##   ##
+ ## ##   ######    ## ##   ##   ##   ##  ##  ##   ##  ##   ##
+ ##  ##  ##  ##    ##  ##  ##   ##   ## ##   ##   ##  ##   ##
+#### ##  ##  ##   ###  ##   #####   #####     #####    #####   v${packageInfo.version} / (c)2021 Hiro527
+`)
+
+const download = async () => {
+    const videoInfo = await ytdl.getInfo(url);
+    const date = new Date();
+    const fileName = `${videoInfo.videoDetails.title}_${date.getFullYear()}${date.getMonth() + 1}${date.getDate()}${date.getHours()}${date.getMinutes()}${date.getSeconds()}`;
+    const videoSec = videoInfo.videoDetails.lengthSeconds;
+    console.log('%s | %s - %s', videoInfo.videoDetails.title, videoInfo.videoDetails.author.name, `${('00' + Math.floor(videoSec / 3600)).slice(-2)}:${('00' + Math.floor(videoSec / 60) % 60).slice(-2)}:${('00' + (videoSec % 60) % 60).slice(-2)}`)
+    let startTime = 0;
+    const tracker = {
+        video: { downloaded: 0, total: 0 },
+        audio: { downloaded: 0, total: 0 }
+    }
+    const video = ytdl(url, { filter: (format) => format.container === 'mp4', quality: 'highestvideo' })
+    const audio = ytdl(url, { quality: 'highestaudio' })
+    video.once('response', () => startTime = Date.now());
+    video.on('progress', (chunkLength, downloaded, total) => {
+        tracker.video.downloaded = downloaded;
+        tracker.video.total = total;
+    })
+    audio.on('progress', (chunkLength, downloaded, total) => {
+        tracker.audio.downloaded = downloaded;
+        tracker.audio.total = total;
+    })
+    const ffmpeg = child_process.spawn('ffmpeg', [
+        '-loglevel', '8', '-hide_banner',
+        '-progress', 'pipe:3',
+        '-i', 'pipe:4',
+        '-i', 'pipe:5',
+        '-map', '0:a',
+        '-map', '1:v',
+        '-c:v', 'copy',
+        `output/${fileName}.mp4`,
+    ], {
+        windowsHide: true,
+        stdio: [
+            'inherit', 'inherit', 'inherit',
+            'pipe', 'pipe', 'pipe',
+        ],
+    })
+    ffmpeg.on('close', () => {
+        clearInterval(trackerInterval);
+        console.log(`\n[INFO] ダウンロードが完了しました: ./output/${fileName}.mp4`);
+        if (mode === 'mp3') {
+            console.log('[INFO] mp3への変換を開始します')
+            const ffmpegMp3 = child_process.spawn('ffmpeg', [
+                '-loglevel', '8', '-hide_banner',
+                '-i', `output/${fileName}.mp4`,
+                '-vn',
+                '-ab', '320k',
+                '-ac', '2',
+                '-ar', '44100',
+                '-acodec', 'libmp3lame',
+                '-f', 'mp3', `output/${fileName}.mp3`
+            ])
+            ffmpegMp3.on('close', () => {
+                console.log(`[INFO] mp3への変換が完了しました: ./output/${fileName}.mp3`);
+            })
+        }
+    });
+    const trackerInterval = setInterval(() => {
+        readline.cursorTo(process.stdout, 0);
+        const timePassed = Math.floor((Date.now() - startTime) / 1000);
+        const downloaded = tracker.audio.downloaded + tracker.video.downloaded;
+        const total = tracker.audio.total + tracker.video.total;
+        const progress = Math.floor(downloaded / total * 100)
+        process.stdout.write(`${progress}% | ${Math.floor(downloaded / 1024 / 1024)}MB / ${Math.floor(total / 1024 / 1024)}MB - ${('00' + Math.floor(timePassed / 3600)).slice(-2)}:${('00' + Math.floor(timePassed / 60) % 60).slice(-2)}:${('00' + (timePassed % 60) % 60).slice(-2)}`);
+        readline.moveCursor(process.stdout, 0);
+    }, 100)
+    audio.pipe(ffmpeg.stdio[4])
+    video.pipe(ffmpeg.stdio[5])
+}
+
+if (ytdl.validateURL(url)) {
+    download()
+}
+else {
+    console.error('[ERROR] 無効なURL: %s', url);
+    exit(1);
+}
